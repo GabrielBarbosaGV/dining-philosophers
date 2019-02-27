74543d38605a0d053f33b3e3830b86033d95fa1a
Cris Correa <ecmac@cin.ufpe.br>
2019-02-27 11:18:37 -0300

    Vai dar deadlock mesmo?

--- withDeadlock.go
+++ withDeadlock.go
@@ -6,36 +6,46 @@ import (
 )
 
 func eat(left chan int, right chan int, id int) {
-	fork1 := <-left
-	fmt.Printf("Philosopher %d got fork %d\n", id, fork1)
-	fork2 := <-right
-	fmt.Printf("Philosopher %d got fork %d\n", id, fork2)
 
-	fmt.Printf("Philosopher %d eating for 10 seconds\n", id)
-	time.Sleep(10 * time.Second)
-	left <- fork1
-	right <- fork2
-
-	//think()
+	for {
+		fork1 := <-left
+		fmt.Printf("Philosopher %d gets fork %d\n", id, fork1)
+		fork2 := <-right
+		fmt.Printf("Philosopher %d gets fork %d\n", id, fork2)
+	
+		fmt.Printf("PHILOSOPHER %d WILL EAT FOR 5 SECONDS\n", id)
+		time.Sleep(10 * time.Second)
+	
+		fmt.Printf("Philosopher %d puts down fork %d\n", id, fork1)
+		left <- fork1
+	
+		fmt.Printf("Philosopher %d puts down fork %d\n", id, fork2)
+		right <- fork2
+	}
+	
 }
 
 func main() {
+
+	fmt.Println("DINNER STARTS\n")
+
 	ch12 := make(chan int)
-	ch12 <- 1
 	ch23 := make(chan int)
-	ch23 <- 2
 	ch34 := make(chan int)
-	ch34 <- 3
 	ch45 := make(chan int)
-	ch45 <- 4
 	ch51 := make(chan int)
-	ch51 <- 5
 
 	go eat(ch12, ch23, 2)
 	go eat(ch23, ch34, 3)
 	go eat(ch34, ch45, 4)
 	go eat(ch45, ch51, 5)
 	go eat(ch51, ch12, 1)
+
+	ch12 <- 1
+	ch23 <- 2
+	ch34 <- 3
+	ch45 <- 4
+	ch51 <- 5
 
 	//close(ch12)
 	//close(ch23)
@@ -44,4 +54,5 @@ func main() {
 	//close(ch51)
 
 	fmt.Scanln()
+	
 }
